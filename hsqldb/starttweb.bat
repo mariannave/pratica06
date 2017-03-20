6882fad831a983a824bc3e6156b0dacc8f4af7e0
Marianna Veríssimo <marianna1204@gmail.com>
2017-03-20 10:54:47 -0300

    first

---
+++ hsqldb/starttweb.bat
@@ -0,0 +1,8 @@
+﻿@echo off
+rem Modifique a variável abaixo para o local onde está a pasta do seu banco de dados.
+set BD_HOME=d:\java\bd
+echo =========
+echo Local do banco de dados: %C:\Users\Marianna\Documents\Prática-JSF-06.Recursos\praticajsf06\bd%
+echo =========
+
+java -cp .\lib\hsqldb.jar org.hsqldb.server.Server --database.0 file:%BD_HOME%\turmasweb\turmasweb --dbname.0 tweb
---
+++ src/main/java/br/edu/ifpb/pweb/turmas/bean/TurmasBean.java
@@ -0,0 +1,144 @@
+package br.edu.ifpb.pweb.turmas.bean;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+import javax.faces.event.ActionEvent;
+
+import br.pweb.turmas.dao.AlunoDAO;
+import br.pweb.turmas.dao.PersistenceUtil;
+import br.pweb.turmas.dao.TurmaDAO;
+import br.pweb.turmas.model.Aluno;
+import br.pweb.turmas.model.Turma;
+
+@ManagedBean
+@ViewScoped
+public class TurmasBean {
+	private List<Turma> turmas;
+	Turma turma;
+	TurmaDAO turmaDao;
+	AlunoDAO alunoDao;
+	private Long turmaId;
+	Aluno aluno;
+	private Flash flash;
+	
+	@PostConstruct
+	public void init(){
+		this.turma = new Turma();
+		this.aluno = new Aluno();
+	}
+	
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	public String cadastrar(){
+		System.out.print("Cadastrando");
+		turmaDao = new TurmaDAO();
+		turmaDao.beginTransaction();
+		turmaDao.insert(this.turma);
+		turmaDao.commit();
+		return "turmas?faces-redirect=true";
+	}
+	
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+
+	public List<Turma> getTurmas() {
+		return turmas;
+	}
+
+	public void setTurmas(List<Turma> turmas) {
+		this.turmas = turmas;
+	}
+	
+	public void listarTurmas() {
+		TurmaDAO tDao = new TurmaDAO();
+		this.turmas = tDao.findAll();
+	}
+	
+	public void atualizarTurmas(ActionEvent e) {
+		this.listarTurmas();
+	}
+	
+	public void selecionar(){
+		System.out.println("Chamando nova página");
+		turmaDao = new TurmaDAO(PersistenceUtil.getCurrentEntityManager());
+		this.turma = turmaDao.find(this.turmaId);
+		loadFlash();
+	}
+
+	public Long getTurmaId() {
+		return turmaId;
+	}
+
+	public void setTurmaId(Long turmaId) {
+		this.turmaId = turmaId;
+	}
+	
+	public String matricular() {
+		System.out.println("Matriculando Aluno");
+		alunoDao = new AlunoDAO();
+		turmaDao = new TurmaDAO();
+		this.aluno.setTurma(this.turma);
+		this.turma.addAluno(this.aluno);
+
+		alunoDao.beginTransaction();
+		alunoDao.insert(this.aluno);
+		alunoDao.commit();
+
+		turmaDao.beginTransaction();
+		turmaDao.update(this.turma);
+		turmaDao.commit();
+
+		return String.format("turma?id=%d&faces-redirect=true", this.turma.getId());
+
+	}
+	
+	public String excluir(Turma turma){
+		System.out.println("Deletando a turma");
+		turmaDao = new TurmaDAO(PersistenceUtil.getCurrentEntityManager());
+		turmaDao.beginTransaction();
+		turmaDao.delete(turma);
+		turmaDao.commit();
+		
+		this.turmas.remove(turma);		
+		return "turmas?faces-redirect=true";
+	}
+	
+	
+	public String excluirAluno(Aluno aluno){
+		System.out.println("Deletando aluno");
+		
+		AlunoDAO dao = new AlunoDAO();
+		dao.beginTransaction();
+		dao.delete(aluno);
+		dao.commit();
+		return "turma?faces-redirect=true";
+	}
+	
+	private void loadFlash(){
+		Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+		flash.put("turma", turma);
+		flash.put("turmas", turmas);
+	}
+	
+	public void unloadFlash(){
+		Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+		this.setTurma((Turma) flash.get(turma));
+		this.setTurmas((List<Turma>) flash.get(turmas));
+	}
+}
